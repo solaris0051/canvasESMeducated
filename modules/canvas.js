6f2da2b2fb5afc90d22db741945ceedf1166f9d0
Y K <ultraman0713@gmail.com>
2022-01-31 20:38:10 +0900

    origin

---
+++ modules/canvas.js
@@ -0,0 +1,45 @@
+// preparing canvas.
+class Canvas {
+  constructor(id, parent, width, height) {
+    this.id = id;
+    this.listId = null;
+    this.parent = parent;
+    this.width = width;
+    this.height = height;
+    this.ctx = null;
+  }
+
+  create() {
+    if(this.ctx !== null) {
+      return;
+    } else {
+      let divWrapper = document.createElement('div');
+      let canvasElem = document.createElement('canvas');
+      this.parent.appendChild(divWrapper);
+      divWrapper.appendChild(canvasElem);
+
+      divWrapper.id = this.id;
+      canvasElem.width = this.width;
+      canvasElem.height = this.height;
+
+      this.ctx = canvasElem.getContext('2d');
+    }
+  }
+
+  createReportList() {
+    if(this.listId !== null) {
+      return;
+    } else {
+      let list = document.createElement('ul');
+      list.id = this.id + '-reporter';
+
+      let canvasWrapper = document.getElementById(this.id);
+      canvasWrapper.appendChild(list);
+
+      this.listId = list.id;
+    }
+  }
+}
+
+// exporing.
+export { Canvas };
---
+++ modules/defColors.js
@@ -0,0 +1,11 @@
+// defining colors
+
+const colors = {
+	"yellow": "#FFFF00",
+	"green": "#008000",
+	"blue": "#0000FF",
+	"red": "#FF0000",
+	"orange": "#FFA500"
+};
+// exporing.
+export default colors;
