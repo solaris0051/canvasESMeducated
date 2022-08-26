6c7ea39f4f0b4f46ad0b5e1ea5c85f132080dc84
Y K <docodoco.0713@outlook.com>
2022-08-26 18:40:51 +0900

    bundl & action checked...

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   devServer: {
     static: {
-      directory: path.join(__dirname, "./dist"),
+      directory: path.join(__dirname, "./"),
     },
     open: true,
     port: 3000,
