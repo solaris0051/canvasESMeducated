398a0e6e2e33206f59782be8810193aeffa1a598
Y K <docodoco.0713@outlook.com>
2022-12-05 21:51:32 +0900

    split and gather canvas

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 //object of color palette
 import colors from "./modules/defColors.js";
 //Classes of Canvases
-import { Canvas, AltCanvas, SubCanvas, YetCanvas, YetAnotherCanvas } from "./modules/canvas.js";
+import {
+  Canvas,
+  AltCanvas,
+  SubCanvas,
+  YetCanvas,
+  YetAnotherCanvas,
+} from "./modules/canvas.js";
 const btn = [
   document.querySelector(".square"),
   document.querySelector(".circle"),
@@ -10,21 +16,24 @@ const btn = [
   document.querySelector(".action"),
   document.querySelector(".clear_canvas1_4"),
   document.querySelector(".clear_canvas5"),
+  document.querySelector(".splitter"),
   document.querySelector(".relo"),
 ];
 
-// one edge of square canvas
+// length of an edge of a canvas
 const edge_of_canvas = 300;
-const Canv1 = new Canvas(`${edge_of_canvas}`, `${edge_of_canvas}`);
-Canv1.create();
-const Canv2 = new AltCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`);
-Canv2.create();
-const Canv3 = new SubCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`);
-Canv3.create();
-const Canv4 = new YetCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`);
-Canv4.create();
-const Canv5 = new YetAnotherCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`);
-Canv5.create();
+
+// creat canvases
+const canv = [
+  new Canvas(`${edge_of_canvas}`, `${edge_of_canvas}`),
+  new AltCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`),
+  new SubCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`),
+  new YetCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`),
+  new YetAnotherCanvas(`${edge_of_canvas}`, `${edge_of_canvas}`),
+];
+for (let i = 0; i < canv.length; i++) {
+  canv[i].create();
+}
 
 // dynamic import(drawing squares on the canvas)
 btn[0].addEventListener(
@@ -33,38 +42,38 @@ btn[0].addEventListener(
     import("./modules/square.js").then((Module) => {
       const square = [
         new Module.Square(
-          Canv1.ctx,
-          Math.floor(Canv1.width / 8),
+          canv[0].ctx,
+          Math.floor(canv[0].width / 8),
           0,
           0,
           colors.purple
         ),
         new Module.Square(
-          Canv1.ctx,
-          Math.floor(Canv1.width / 6.4),
-          Math.floor(Canv1.width / 8),
-          Math.floor(Canv1.width / 8),
+          canv[0].ctx,
+          Math.floor(canv[0].width / 6.4),
+          Math.floor(canv[0].width / 8),
+          Math.floor(canv[0].width / 8),
           colors.yellow
         ),
         new Module.Square(
-          Canv1.ctx,
-          Math.floor(Canv1.width / 5.128),
-          Math.floor(Canv1.width / 3.555),
-          Math.floor(Canv1.width / 3.555),
+          canv[0].ctx,
+          Math.floor(canv[0].width / 5.128),
+          Math.floor(canv[0].width / 3.555),
+          Math.floor(canv[0].width / 3.555),
           colors.purple
         ),
         new Module.Square(
-          Canv1.ctx,
-          Math.floor(Canv1.width / 4.102),
-          Math.floor(Canv1.width / 2.099),
-          Math.floor(Canv1.width / 2.099),
+          canv[0].ctx,
+          Math.floor(canv[0].width / 4.102),
+          Math.floor(canv[0].width / 2.099),
+          Math.floor(canv[0].width / 2.099),
           colors.yellow
         ),
         new Module.Square(
-          Canv1.ctx,
-          Math.floor(Canv1.width / 3.587),
-          Math.floor(Canv1.width / 1.388),
-          Math.floor(Canv1.width / 1.388),
+          canv[0].ctx,
+          Math.floor(canv[0].width / 3.587),
+          Math.floor(canv[0].width / 1.388),
+          Math.floor(canv[0].width / 1.388),
           colors.purple
         ),
       ];
@@ -84,74 +93,74 @@ btn[1].addEventListener(
       // greenBlue
       const circle = [
         new Module.Circle(
-          Canv2.ctx,
-          Math.floor(Canv2.width / 4),
-          Math.floor(Canv2.width / 2),
-          Math.floor(Canv2.width / 1.333),
+          canv[1].ctx,
+          Math.floor(canv[1].width / 4),
+          Math.floor(canv[1].width / 2),
+          Math.floor(canv[1].width / 1.333),
           colors.green
         ),
         new Module.Circle(
-          Canv2.ctx,
-          Math.floor(Canv2.width / 4.923),
-          Math.floor(Canv2.width / 2),
-          Math.floor(Canv2.width / 1.254),
+          canv[1].ctx,
+          Math.floor(canv[1].width / 4.923),
+          Math.floor(canv[1].width / 2),
+          Math.floor(canv[1].width / 1.254),
           colors.blue
         ),
         new Module.Circle(
-          Canv2.ctx,
-          Math.floor(Canv2.width / 6.4),
-          Math.floor(Canv2.width / 2),
-          Math.floor(Canv2.width / 1.185),
+          canv[1].ctx,
+          Math.floor(canv[1].width / 6.4),
+          Math.floor(canv[1].width / 2),
+          Math.floor(canv[1].width / 1.185),
           colors.green
         ),
         new Module.Circle(
-          Canv2.ctx,
-          Math.floor(Canv2.width / 9.142),
-          Math.floor(Canv2.width / 2),
-          Math.floor(Canv2.width / 1.122),
+          canv[1].ctx,
+          Math.floor(canv[1].width / 9.142),
+          Math.floor(canv[1].width / 2),
+          Math.floor(canv[1].width / 1.122),
           colors.blue
         ),
         new Module.Circle(
-          Canv2.ctx,
-          Math.floor(Canv2.width / 16),
-          Math.floor(Canv2.width / 2),
-          Math.floor(Canv1.width / 1.066),
+          canv[1].ctx,
+          Math.floor(canv[1].width / 16),
+          Math.floor(canv[1].width / 2),
+          Math.floor(canv[1].width / 1.066),
           colors.green
         ),
         //redPurple
         new Module.Circle(
-          Canv3.ctx,
-          Math.floor(Canv3.width / 4),
-          Math.floor(Canv3.width / 1.33),
-          Math.floor(Canv3.width / 1.333),
+          canv[2].ctx,
+          Math.floor(canv[2].width / 4),
+          Math.floor(canv[2].width / 1.33),
+          Math.floor(canv[2].width / 1.333),
           colors.red
         ),
         new Module.Circle(
-          Canv3.ctx,
-          Math.floor(Canv3.width / 4.923),
-          Math.floor(Canv3.width / 1.33),
-          Math.floor(Canv3.width / 1.254),
+          canv[2].ctx,
+          Math.floor(canv[2].width / 4.923),
+          Math.floor(canv[2].width / 1.33),
+          Math.floor(canv[2].width / 1.254),
           colors.purple
         ),
         new Module.Circle(
-          Canv3.ctx,
-          Math.floor(Canv3.width / 6.4),
-          Math.floor(Canv3.width / 1.33),
-          Math.floor(Canv3.width / 1.185),
+          canv[2].ctx,
+          Math.floor(canv[2].width / 6.4),
+          Math.floor(canv[2].width / 1.33),
+          Math.floor(canv[2].width / 1.185),
           colors.red
         ),
         new Module.Circle(
-          Canv3.ctx,
-          Math.floor(Canv3.width / 9.142),
-          Math.floor(Canv3.width / 1.33),
-          Math.floor(Canv3.width / 1.122),
+          canv[2].ctx,
+          Math.floor(canv[2].width / 9.142),
+          Math.floor(canv[2].width / 1.33),
+          Math.floor(canv[2].width / 1.122),
           colors.purple
         ),
         new Module.Circle(
-          Canv3.ctx,
-          Math.floor(Canv3.width / 16),
-          Math.floor(Canv3.width / 1.33),
-          Math.floor(Canv3.width / 1.066),
+          canv[2].ctx,
+          Math.floor(canv[2].width / 16),
+          Math.floor(canv[2].width / 1.33),
+          Math.floor(canv[2].width / 1.066),
           colors.red
         ),
       ];
@@ -170,45 +179,45 @@ btn[2].addEventListener(
     import("./modules/triangle.js").then((Module) => {
       const triangle = [
         new Module.Triangle(
-          Canv4.ctx,
-          Math.floor(Canv4.width / 7.079),
-          Math.floor(Canv4.width / 1.164),
+          canv[3].ctx,
+          Math.floor(canv[3].width / 7.079),
+          Math.floor(canv[3].width / 1.164),
           0,
           colors.red
         ),
         new Module.Triangle(
-          Canv4.ctx,
-          Math.floor(Canv4.width / 7.079),
-          Math.floor(Canv4.width / 1.393),
+          canv[3].ctx,
+          Math.floor(canv[3].width / 7.079),
+          Math.floor(canv[3].width / 1.393),
           0,
           colors.red
         ),
         new Module.Triangle(
-          Canv4.ctx,
-          Math.floor(Canv4.width / 7.079),
-          Math.floor(Canv4.width / 1.735),
+          canv[3].ctx,
+          Math.floor(canv[3].width / 7.079),
+          Math.floor(canv[3].width / 1.735),
           0,
           colors.red
         ),
         new Module.Triangle(
-          Canv4.ctx,
-          Math.floor(Canv4.width / 7.079),
-          Math.floor(Canv4.width / 1.268),
-          Math.floor(Canv4.width / 8.107),
+          canv[3].ctx,
+          Math.floor(canv[3].width / 7.079),
+          Math.floor(canv[3].width / 1.268),
+          Math.floor(canv[3].width / 8.107),
           colors.orange
         ),
         new Module.Triangle(
-          Canv4.ctx,
-          Math.floor(Canv4.width / 7.079),
-          Math.floor(Canv4.width / 1.545),
-          Math.floor(Canv4.width / 8.107),
+          canv[3].ctx,
+          Math.floor(canv[3].width / 7.079),
+          Math.floor(canv[3].width / 1.545),
+          Math.floor(canv[3].width / 8.107),
           colors.orange
         ),
         new Module.Triangle(
-          Canv4.ctx,
-          Math.floor(Canv4.width / 7.079),
-          Math.floor(Canv4.width / 1.393),
-          Math.floor(Canv4.width / 4.085),
+          canv[3].ctx,
+          Math.floor(canv[3].width / 7.079),
+          Math.floor(canv[3].width / 1.393),
+          Math.floor(canv[3].width / 4.085),
           colors.red
         ),
       ];
@@ -267,5 +276,18 @@ btn[5].addEventListener(
   false
 );
 
+// splitting_gathering_canvases
+btn[6].addEventListener(
+  "click",
+  () => {
+    document.getElementById("canvas1").classList.toggle("canvas");
+    document.getElementById("canvas2").classList.toggle("canvas");
+    document.getElementById("canvas3").classList.toggle("canvas");
+    document.getElementById("canvas4").classList.toggle("canvas");
+    document.getElementById("canvas5").classList.toggle("canvas");
+  },
+  false
+);
+
 //reloading
-btn[6].addEventListener("click", () => location.reload(), false);
+btn[7].addEventListener("click", () => location.reload(), false);
